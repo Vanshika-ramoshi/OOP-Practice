6f89c5d46d6379c050e7fefd25a69cecfd99e2a7
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:55:06 +0530

    Add Book class with title, author, and price

---
+++ Book/Book.java
@@ -0,0 +1,14 @@
+package book;
+
+public class Book {
+
+    String title;
+    String author;
+    double price;
+
+    void showBook() {
+        System.out.println("Title : " + title);
+        System.out.println("Author : " + author);
+        System.out.println("Price : " + price);
+    }
+}
