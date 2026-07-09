9564c6f52145384f2094c3002448f75fabc53721
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:48:21 +0530

    Add MainClass to initialize ATM and check balance

---
+++ ATM/MainClass.java
@@ -0,0 +1,13 @@
+package employees;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        ATM a = new ATM();
+
+        a.balance = 25000;
+
+        a.checkBalance();
+    }
+}
