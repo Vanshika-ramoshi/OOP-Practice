d58404cad388d6aae7f3bdd75deb24cda973575c
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:34:26 +0530

    Add Student class with display method

---
+++ Student/Student.java
@@ -0,0 +1,14 @@
+package objects;
+
+class Student {
+
+    String name;
+    int rollNo;
+    double marks;
+
+    void display() {
+        System.out.println("Name : " + name);
+        System.out.println("Roll No : " + rollNo);
+        System.out.println("Marks : " + marks);
+    }
+}
