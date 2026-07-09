ef7f0a0e0c2b15b63b31ee71eae99273c9b392bb
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:59:37 +0530

    Create BankAccount.java

---
+++ BankAccount/BankAccount.java
@@ -0,0 +1,12 @@
+package bank;
+
+public class BankAccount {
+
+    int accountNumber;
+    double balance;
+
+    void displayBalance() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Balance : " + balance);
+    }
+}
