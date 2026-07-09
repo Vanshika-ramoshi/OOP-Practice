74cef1e7cb2f2042a4b1ad71baa603187aceff9e
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 19:05:33 +0530

    Add Circle class with area calculation method

---
+++ Circle/Circle.java
@@ -0,0 +1,12 @@
+package class;
+
+public class Circle {
+
+    double radius;
+
+    void area() {
+        double area = 3.14 * radius * radius;
+        System.out.println("Radius : " + radius);
+        System.out.println("Area : " + area);
+    }
+}
