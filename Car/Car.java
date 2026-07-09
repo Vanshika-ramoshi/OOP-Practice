5186ce9d9949958f2ac9d0da89ea309692544040
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:44:13 +0530

    Add Car class with attributes and method

---
+++ Car/Car.java
@@ -0,0 +1,13 @@
+package car;
+	public class Car {
+
+	    String brand;
+	    String model;
+	    double price;
+
+	    void showCar() {
+	        System.out.println("Brand : " + brand);
+	        System.out.println("Model : " + model);
+	        System.out.println("Price : " + price);
+	    }
+	}
