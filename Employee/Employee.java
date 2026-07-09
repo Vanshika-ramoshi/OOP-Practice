3fe9a8f9fb16738174fc221656734e2cf55f1803
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 18:38:04 +0530

    Add Employee class with details display method

---
+++ Employee/Employee.java
@@ -0,0 +1,13 @@
+package employees;
+
+public class Employee {
+
+		String name;
+		double salary;
+
+		void showDetails() {
+			System.out.println("Employee Name : " + name);
+			System.out.println("Salary : " + salary);
+		}
+}
+	
