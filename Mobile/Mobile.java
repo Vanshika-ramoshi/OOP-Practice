77ad3568dc0a0f52301de0b28da1b438dc9eafdb
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-09 19:10:23 +0530

    Add Mobile class with specifications display method

---
+++ Mobile/Mobile.java
@@ -0,0 +1,14 @@
+package mobile;
+
+public class Mobile {
+
+    String brand;
+    int ram;
+    double price;
+
+    void displaySpec() {
+        System.out.println("Brand : " + brand);
+        System.out.println("RAM : " + ram + " GB");
+        System.out.println("Price : " + price);
+    }
+}
